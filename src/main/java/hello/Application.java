144560a4024d6b44c1938fc73b3ac6f88edb7665
Yousuf Nejati <yousufnejati@gmail.com>
2016-08-23 22:07:54 -0700

    Add greeting View, for real this time

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by you on 8/23/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
